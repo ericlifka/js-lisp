6f5c10fb10bfb2d4b3ae244775b3970b22198ca9
Eric Lifka <eric.lifka@gmail.com>
2014-11-25 15:32:33 -0500

    simple proof of concept test for lists

---
+++ test/list_tests.js
@@ -0,0 +1,12 @@
+/*global require,describe,it*/
+
+var should = require("should");
+var list = require('../src/list');
+
+describe('list', function(){
+    describe('cons', function(){
+        it('should return an object', function(){
+            list.cons().should.be.an.instanceOf(Object);
+        });
+    });
+});
