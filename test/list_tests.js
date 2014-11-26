08317c32770601af779c6b26ec8a7ed833013d81
Eric Lifka <eric.lifka@gmail.com>
2014-11-25 23:30:35 -0500

    some tests for list functionality

--- test/list_tests.js
+++ test/list_tests.js
@@ -2,10 +2,19 @@
 var should = require('should');
 var list = require('../src/list');
 
-describe('list', function(){
-    describe('cons', function(){
-        it('should return an object', function(){
-            list.cons().should.be.an.instanceOf(Object);
+describe('list', function () {
+    describe('cons', function () {
+        it('should create an empty list', function (){
+            var li = list.cons();
+            li.should.be.an.instanceOf(Object);
+            (li.car === undefined).should.be.ok;
+            (li.cdr === undefined).should.be.ok;
+        });
+
+        it('should store the value in the car', function () {
+            var li = list.cons(5);
+            li.car.should.be.exactly(5);
+            (li.cdr === undefined).should.be.ok;
         });
     });
 });
