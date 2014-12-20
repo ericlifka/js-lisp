fb02d307012cedb59f1ce92901bfccb572f4bd35
Eric Lifka <eric.lifka@gmail.com>
2014-12-19 22:46:43 -0500

    spec out Parser methods

--- src/parser.js
+++ src/parser.js
@@ -2,6 +2,18 @@ function Parser() {
 
 }
 
+Parser.prototype = {
+    addString: function (string) {
+
+    },
+    parseState: function () {
+
+    },
+    getLists: function () {
+
+    }
+};
+
 module.exports = {
     Parser: Parser
 };
