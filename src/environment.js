cb6793472a87f5549beb2e6ef91fe7115b6a2e25
Eric Lifka <eric.lifka@gmail.com>
2014-12-30 23:05:18 -0500

    first crappy pass at builtins, just to get something into the environment to play around with

--- src/environment.js
+++ src/environment.js
@@ -8,6 +8,16 @@ function create() {
     return new Environment();
 }
 
+function addBuiltins(env) {
+    env.symbols['+'] = function () {
+        var sum = 0;
+        for (var i = 0; i < arguments.length; i++) {
+            sum += arguments[i];
+        }
+        return sum;
+    };
+}
+
 module.exports = {
     create: create
 };
