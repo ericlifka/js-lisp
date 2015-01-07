a12748d3895c65224f44868591a278c19facef5f
Eric Lifka <eric.lifka@gmail.com>
2015-01-06 22:00:13 -0500

    error class for evaluation errors

--- src/error.js
+++ src/error.js
@@ -1,4 +1,5 @@
 module.exports = {
     ParseError: function (message) { this.message = message; },
-    ListError: function (message) { this.message = message; }
+    ListError: function (message) { this.message = message; },
+    EvaluationError: function (message) { this.message = message; }
 };
