686f18a5bfa2ca8eb39a4908864b675f804945dc
Eric Lifka <eric.lifka@gmail.com>
2015-01-13 23:40:55 -0500

    working towards function creation

--- src/special-forms.js
+++ src/special-forms.js
@@ -2,11 +2,22 @@ var List = require('./list');
 
 module.exports = {
     "fn": List.special(function (scopeEnvironment, list, callback) {
-        callback(List.error("Not Implemented"));
+        if (!list || list.length() < 2) {
+            return callback(List.error("Invalid lambda, must be of the form `(fn (...arguments) ...body)`"));
+        }
+
+        var formals = list.car;
+        var body = list.cdr;
+
+        callback(List.func(function (parameters, innerCallback) {
+            innerCallback(List.error("Not Implemented"));
+        }));
     }),
+
     "def-fn": List.special(function (list, callback) {
         callback(List.error("Not Implemented"));
     }),
+
     "def-macro": List.special(function (list, callback) {
         callback(List.error("Not Implemented"));
     })
