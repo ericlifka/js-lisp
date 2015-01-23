6479e1b1831eb38f0bf84f88ca4399a3e61794e2
Eric Lifka <eric.lifka@gmail.com>
2015-01-22 23:02:56 -0500

    expose cons to the language

--- src/constructs/lists.js
+++ src/constructs/lists.js
@@ -3,5 +3,17 @@ var List = require('./../list');
 module.exports = {
     "list": List.func(function (parameters, callback) {
         callback(parameters);
+    }),
+
+    "cons": List.func(function (parameters, callback) {
+        if (!parameters) {
+            callback(null);
+        }
+        else if (parameters.length() === 1) {
+            callback(List.cons(parameters.car));
+        }
+        else {
+            callback(List.cons(parameters.car, parameters.cdr.car));
+        }
     })
 };
