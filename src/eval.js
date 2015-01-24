a4441fce6bf24111216255c82ebab5b6d96917db
Eric Lifka <eric.lifka@gmail.com>
2015-01-24 13:32:33 -0500

    add null to types that resolve to themselves

--- src/eval.js
+++ src/eval.js
@@ -18,6 +18,7 @@ function evaluateStatement(statement, environment, callback) {
         case 'error':
         case 'string':
         case 'number':
+        case 'null':
             return statement;
 
         default:
