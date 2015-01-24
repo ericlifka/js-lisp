77a6bd9487c8e9b0aab3e0377ffb134b9cc772c3
Eric Lifka <eric.lifka@gmail.com>
2015-01-24 14:03:24 -0500

    math module

--- src/constructs/math.js
+++ src/constructs/math.js
@@ -1,59 +1,57 @@
 var List = require('./../list');
 
-function combineNumbers(base, parameters, combinator, callback) {
+function combineNumbers(base, parameters, combinator) {
     if (!List.isNumber(base)) {
-        return callback(List.error("Encountered non numeric value in mathematical operator: '" + base + "'"));
+        return List.error("Encountered non numeric value in mathematical operator: '" + base + "'");
     }
 
     var aggregate = base.value;
     while (parameters) {
         if (!List.isNumber(parameters.car)) {
-            return callback(List.error("Encountered non numeric value in mathematical operator: '" + parameters.car + "'"));
+            return List.error("Encountered non numeric value in mathematical operator: '" + parameters.car + "'");
         }
         aggregate = combinator(aggregate, parameters.car.value);
         parameters = parameters.cdr;
     }
-    callback(List.number(aggregate));
+    return List.number(aggregate);
 };
 
-function safeCombine(operator, parameters, monad, combinator, callback) {
+function safeCombine(operator, parameters, monad, combinator) {
     if (!parameters) {
-        callback(List.error(operator + " needs at least 1 argument"));
+        return List.error(operator + " needs at least 1 argument");
     }
     else if (parameters.length() === 1 && List.isNumber(parameters.car)) {
-        callback(List.number(monad(parameters.car.value)));
+        return List.number(monad(parameters.car.value));
     }
     else {
-        combineNumbers(parameters.car, parameters.cdr, combinator, callback);
+        return combineNumbers(parameters.car, parameters.cdr, combinator);
     }
 }
 
 module.exports = {
-    "+": List.func(function (parameters, callback) {
-        combineNumbers(List.number(0), parameters, function (a, b) { return a + b; }, callback);
+    "+": List.func(function (parameters) {
+        return combineNumbers(List.number(0), parameters, function (a, b) { return a + b; });
     }),
 
-    "*": List.func(function (parameters, callback) {
-        combineNumbers(List.number(1), parameters, function (a, b) { return a * b; }, callback);
+    "*": List.func(function (parameters) {
+        return combineNumbers(List.number(1), parameters, function (a, b) { return a * b; });
     }),
 
-    "-": List.func(function (parameters, callback) {
-        safeCombine(
+    "-": List.func(function (parameters) {
+        return safeCombine(
             "-",
             parameters,
             function (a) { return -a; },
-            function (a, b) { return a - b; },
-            callback
+            function (a, b) { return a - b; }
         );
     }),
 
-    "/": List.func(function (parameters, callback) {
-        safeCombine(
+    "/": List.func(function (parameters) {
+        return safeCombine(
             "/",
             parameters,
             function (a) { return 1 / a; },
-            function (a, b) { return a / b; },
-            callback
+            function (a, b) { return a / b; }
         );
     })
 };
