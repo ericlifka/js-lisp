159487c58868090c6bedbbb574ca6054e17115a1
Eric Lifka <eric.lifka@gmail.com>
2015-01-25 22:21:48 -0500

    equals implementation

--- src/constructs/comparison.js
+++ src/constructs/comparison.js
@@ -2,7 +2,14 @@ var List = require('../list');
 
 module.exports = {
     "=": List.func(function (parameters) {
+        // TODO: Only implementing strict equals for now, add fuzzy equals at some point
+        if (!parameters || parameters.length() !== 2) {
+            return List.error("= only supports two arguments");
+        }
 
+        return List.boolean(List.compareCells(parameters.car, parameters.cdr.car, function (valA, valB) {
+            return valA === valB;
+        }));
     }),
 
     ">": List.func(function (parameters) {
