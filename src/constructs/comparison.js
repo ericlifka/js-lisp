d0db2286e6e10f45d6d952cd9ae833aae91e7052
Eric Lifka <eric.lifka@gmail.com>
2015-01-26 22:32:22 -0500

    pattern refactor

--- src/constructs/comparison.js
+++ src/constructs/comparison.js
@@ -1,15 +1,18 @@
 var List = require('../list');
 
+function compare(parameters, symbol, comparisonFn) {
+    if (!parameters || parameters.length() !== 2) {
+        return List.error(symbol + " only supports two arguments");
+    }
+
+    return List.boolean(List.compareCells(parameters.car, parameters.cdr.car, comparisonFn));
+}
+
 module.exports = {
     "=": List.func(function (parameters) {
-        // TODO: Only implementing strict equals for now, add fuzzy equals at some point
-        if (!parameters || parameters.length() !== 2) {
-            return List.error("= only supports two arguments");
-        }
-
-        return List.boolean(List.compareCells(parameters.car, parameters.cdr.car, function (valA, valB) {
+        return compare(parameters, "=", function (valA, valB) {
             return valA === valB;
-        }));
+        })
     }),
 
     ">": List.func(function (parameters) {
