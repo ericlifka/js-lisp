d43642cbac1d967b09b07c7ce8f6d5516dcc9e58
Eric Lifka <eric.lifka@gmail.com>
2015-02-03 21:23:28 -0500

    pull control flow special forms into their own module

--- src/constructs/index.js
+++ src/constructs/index.js
@@ -3,4 +3,5 @@ exports.lists = require("./lists");
 exports.variables = require("./variables");
 exports.comparison = require("./comparison");
 exports.quotations = require("./quotations");
+exports.controlFlow = require("./control-flow");
 exports.specialForms = require("./special-forms");
--- src/constructs/special-forms.js
+++ src/constructs/special-forms.js
@@ -81,49 +81,5 @@ module.exports = {
 
     "def-macro": List.macro(function (list) {
         return defTransform(list, "macro");
-    }),
-
-    "if": List.special(function (scopeEnvironment, list) {
-        if (!list || list.length < 2) {
-            return List.error("if - invalid structure, expected (if booleanStatement trueStatement ?falseStatement");
-        }
-
-        var boolStatement = list.car;
-        var trueStatement = list.cdr.car;
-        var falseStatement = (list.length() >= 3) ? list.cdr.cdr.car : List.nullValue();
-
-        var boolValue = Eval.evaluateStatement(boolStatement, scopeEnvironment);
-
-        var chosenStatement = List.cellToBool(boolValue) ? trueStatement : falseStatement;
-        return Eval.evaluateStatement(chosenStatement, scopeEnvironment);
-    }),
-
-    "while": List.special(function (scopeEnvironment, list) {
-        if (!list || list.length < 1) {
-            return List.error("while - invalid structure, expected (while booleanStatement ...body?)");
-        }
-
-        var boolStatement = list.car;
-        var bodyStatements = list.cdr;
-        var result = List.nullValue();
-        var boolStatementResult, currentBody, currentBodyStatement;
-
-        while (true) {
-            boolStatementResult = Eval.evaluateStatement(boolStatement, scopeEnvironment);
-            if (!List.cellToBool(boolStatementResult)) {
-                break;
-            }
-
-            currentBody = bodyStatements;
-            while (currentBody) {
-                currentBodyStatement = currentBody.car;
-
-                result = Eval.evaluateStatement(currentBodyStatement, scopeEnvironment);
-
-                currentBody = currentBody.cdr;
-            }
-        }
-
-        return result;
     })
 };
