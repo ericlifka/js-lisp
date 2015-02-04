11d65a9dd5ffa156d356a40dacbd48bf2aef9882
Eric Lifka <eric.lifka@gmail.com>
2015-02-03 21:21:10 -0500

    move variable special forms to their own module

--- src/constructs/index.js
+++ src/constructs/index.js
@@ -1,5 +1,6 @@
 exports.math = require("./math");
 exports.lists = require("./lists");
+exports.variables = require("./variables");
 exports.comparison = require("./comparison");
 exports.quotations = require("./quotations");
 exports.specialForms = require("./special-forms");
--- src/constructs/special-forms.js
+++ src/constructs/special-forms.js
@@ -67,62 +67,6 @@ function defTransform(list, callableSymbol) {
 }
 
 module.exports = {
-    "def": List.special(function (scopeEnvironment, list) {
-        if (scopeEnvironment.readOnly) {
-            return List.error("def - tried to set value in a read only environment");
-        }
-        if (!list || list.length() !== 2) {
-            return List.error("def - takes exactly 2 arguments, a symbol and a value: `(def a 2)`");
-        }
-
-        var symbol = list.car;
-        var statement = list.cdr.car;
-
-        if (!symbol || !symbol.name) {
-            return List.error("Symbol given to def must be valid");
-        }
-        else if (!statement) {
-            return List.error("Statement given to def must be valid");
-        }
-
-        var resultValue = Eval.evaluateStatement(statement, scopeEnvironment);
-        scopeEnvironment.putSymbolValue(symbol.name, resultValue);
-        return resultValue;
-    }),
-
-    "set": List.special(function (scopeEnvironment, list) {
-        if (!list || list.length() !== 2) {
-            return List.error("set - takes exactly 2 arguments, a symbol and a value: `(set a 2)`");
-        }
-
-        var symbol = list.car;
-        var statement = list.cdr.car;
-
-        if (!symbol || !symbol.name) {
-            return List.error("set - symbol not valid");
-        }
-        else if (!statement) {
-            return List.error("set - statement not valid");
-        }
-
-        var env = scopeEnvironment;
-        while (env) {
-            if (env.hasSymbolValue(symbol)) {
-                if (env.readOnly) {
-                    return List.error("set - symbol '" + symbol + "' is defined in a read only environment");
-                }
-                var resultValue = Eval.evaluateStatement(statement, scopeEnvironment);
-                env.putSymbolValue(symbol, resultValue);
-
-                return resultValue;
-            }
-
-            env = env.parent;
-        }
-
-        return List.error("set - symbol not defined in environment chain");
-    }),
-
     "fn": List.special(function (scopeEnvironment, list) {
         return List.func(createCallable(scopeEnvironment, list));
     }),
@@ -178,59 +122,6 @@ module.exports = {
 
                 currentBody = currentBody.cdr;
             }
-        }
-
-        return result;
-    }),
-
-    "let": List.special(function (scopeEnvironment, list) {
-        if (!list || list.length() < 1) {
-            return List.error("let - invalid structure expected `(let (...assignment pairs) ...statements?)`");
-        }
-
-        var assignments = list.car;
-        var statements = list.cdr;
-
-        if (!List.isCons(assignments)) {
-            return List.error("let - second parameter to let must be a list");
-        }
-
-        var localLetScope = Environment.create({parent: scopeEnvironment});
-
-        if (assignments) { // The empty list is a valid let form
-
-            if (assignments.length() % 2 !== 0) {
-                return List.error("let - imbalanced assignments list, must be matched pairs");
-            }
-
-            var assignmentPtr = assignments;
-            var symbol, expression, expressionResult;
-
-            while (assignmentPtr) {
-                symbol = assignmentPtr.car;
-                expression = assignmentPtr.cdr.car;
-
-                if (!List.isSymbol(symbol)) {
-                    return List.error("let - found non symbol in symbol position: '" + symbol + "'");
-                }
-
-                expressionResult = Eval.evaluateStatement(expression, localLetScope);
-                localLetScope.putSymbolValue(symbol, expressionResult);
-
-                assignmentPtr = assignmentPtr.cdr.cdr; // jump forward two positions at a time
-            }
-        }
-
-        var statementsPtr = statements;
-        var result = List.nullValue();
-        var statement;
-
-        while (statementsPtr) {
-            statement = statementsPtr.car;
-
-            result = Eval.evaluateStatement(statement, localLetScope);
-
-            statementsPtr = statementsPtr.cdr;
         }
 
         return result;
